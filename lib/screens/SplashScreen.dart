db2fd66f2b32e8ed005e5f3b309a9048c4c10fb0
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-05-02 21:27:12 +0530

    feat: Created Splash Screen

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text(
+              'A R I S T O',
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Spacer(),
+            SizedBox(height: 20),
+            Text(
+              'Version 1.0',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '© 2024 All rights reserved\nPowered by HIGHSILICON',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 50),
+          ],
+        ),
+      ),
+    );
   }
 }
