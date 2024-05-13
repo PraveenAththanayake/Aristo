f99f336e0edffa464de148d9166771712b0c6edf
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-05-13 17:41:01 +0530

    Changes on the custom color palette

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFFFFFFFF);
-  static const Color titleColor = Color(0x00000000);
-  static const Color textColor = Color(0xff666060);
+  static const Color primaryColor = Color(0x00000000);
+  static const Color secondaryColor = Color(0xff666060);
 }
