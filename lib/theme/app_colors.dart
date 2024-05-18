ba693f41cdacb5c8576a603622468f1d6f248123
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-05-18 21:43:25 +0530

    fix: App colorerror fix

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0x00000000);
-  static const Color secondaryColor = Color(0xff666060);
+  static const Color primaryColor = Color.fromARGB(0, 0, 0, 0);
+  static const Color secondaryColor = Color.fromARGB(255, 102, 96, 96);
 }
